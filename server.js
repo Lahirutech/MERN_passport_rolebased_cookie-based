d46c43642c4041c17cbd555aaea5dde120323d05
Lanka Rathnayake <LankaR@99x.io>
2022-02-21 20:08:46 +0530

    adding the error handler

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
 const express = require("express");
 const morgan = require("morgan")
 const bodyParser = require("body-parser")
+const createHttpError = require('http-errors');
+
 const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
-
 app.use(express.json());
 // app.use(require("./routes/record"));
 // get driver connection
 const dbo = require("./db/connection");
-
 //middleware
 app.use(morgan('dev'))
-
-//Routes
+    //Routes
 app.use('/users', require('./routes/users'))
+
+// 404 Handler
+app.use((req, res, next) => {
+    next(createHttpError.NotFound());
+});
+
+// Error Handler
+app.use((error, req, res, next) => {
+    error.status = error.status || 500;
+    res.status(error.status);
+    res.send(error)
+});
 
 
 
